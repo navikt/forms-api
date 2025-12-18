29ca850b8d00c28bbb164885b8070cfea1585d38
Lars Olav Torvik <lotorvik@torvik.no>
2025-12-18 11:22:24 +0100

    Update static pdf definition

--- src/main/kotlin/no/nav/forms/staticpdfs/StaticPdfController.kt
+++ src/main/kotlin/no/nav/forms/staticpdfs/StaticPdfController.kt
@@ -3,21 +3,22 @@ package no.nav.forms.staticpdfs
 import no.nav.forms.api.StaticPdfApi
 import no.nav.forms.config.AzureAdConfig
 import no.nav.forms.model.StaticPdfDto
+import no.nav.forms.model.UploadPdfResponse
 import no.nav.forms.security.SecurityContextHolder
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.time.OffsetDateTime
 
 @RestController
 @ProtectedWithClaims(issuer = AzureAdConfig.ISSUER, claimMap = ["${AzureAdConfig.CLAIM_NAV_IDENT}=*"])
 class StaticPdfController(private val securityContextHolder: SecurityContextHolder ): StaticPdfApi{
 	private final val logger: Logger = LoggerFactory.getLogger(javaClass)
 
-	@Unprotected
 	override fun getStaticPdfs(
 		formPath: String
 	): ResponseEntity<List<StaticPdfDto>> {
@@ -25,16 +26,27 @@ class StaticPdfController(private val securityContextHolder: SecurityContextHold
 		return ResponseEntity.ok(emptyList())
 	}
 
-	@Unprotected
 	override fun uploadStaticPdf(
 		formPath: String,
-		languageCode: String
-	): ResponseEntity<Unit> {
+		languageCode: String,
+		fileContent: MultipartFile
+	): ResponseEntity<UploadPdfResponse> {
 		logger.info("Upload static pdf for $formPath and language code $languageCode")
-		return ResponseEntity(HttpStatus.OK)
+		securityContextHolder.requireAdminUser()
+		val userId = securityContextHolder.getUserName()
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(
+				UploadPdfResponse(
+					id = 1,
+					languageCode = languageCode,
+					fileName = "test.pdf",
+					createdAt = OffsetDateTime.now(),
+					createdBy = userId
+				)
+			)
 	}
 
-	@Unprotected
 	override fun deleteStaticPdf(
 		formPath: String,
 		languageCode: String
