f5e1d454c07bb2b506d49fc561026bc6947fed4c
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2026-05-20 09:25:24 +0200

    upgrade postgres from 17 to 18

--- src/main/kotlin/no/nav/forms/config/EmbeddedDb.kt
+++ src/main/kotlin/no/nav/forms/config/EmbeddedDb.kt
@@ -10,7 +10,7 @@ import javax.sql.DataSource
 @Configuration
 class EmbeddedDb {
 
-	private var embeddedPostgres: EmbeddedPostgres = EmbeddedPostgres.builder().setTag("17").start()
+	private var embeddedPostgres: EmbeddedPostgres = EmbeddedPostgres.builder().setTag("18").start()
 
 	@Bean
 	fun embeddedPostgres(): DataSource {
